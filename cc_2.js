e71259c6a6a45f12735dfce9c418d031ad0f046d
Jonas <jonashooncantey@gmail.com>
2025-01-26 18:39:28 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -1,3 +1,4 @@
+//Task 1
 console.log("Task One");
 
 let products = ["Keyboard", "Mouse", "Headset", "Desk", "Monitor"]
@@ -7,3 +8,16 @@ products.pop()
 
 
 console.log(products);
+
+
+//Task 2
+console.log("Task Two")
+
+let scores = [80, 59, 90, 93, 74]
+scores [1] = 69
+
+const sum = scores.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+const average = sum / scores.length;
+ 
+console.log(scores)
+console.log(average);
